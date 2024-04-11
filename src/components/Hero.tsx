450e04f54eaf6590d3823c6eeb20bda0587bd009
Ronit Raj <ronitrajofficial7@gmail.com>
2024-04-11 18:55:47 +0530

    prog: hero section done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,62 @@
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useGSAP } from "@gsap/react";
+import { useEffect, useState } from "react";
 export const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideo = () => {
+    if (window.innerHeight < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideo);
+
+    return () => {
+      window.removeEventListener("resize", handleVideo);
+    };
+  }, []);
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 2 });
+    gsap.to("#cta", {
+      y: -50,
+      opacity: 1,
+      delay: 2,
+    });
+  }, []);
   return (
-    <div>
-      <section className="w-full nav-height bg-black relative">
-        <div className="h-5/6 w-full flex-center flex-col">
-          <p className="hero-title">iPhone 15 pro</p>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
         </div>
-      </section>
-    </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From ₹2,500/month or ₹1,49,999</p>
+      </div>
+    </section>
   );
 };
