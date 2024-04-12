28430f5eaf1637448d4c5d637fe5a2b0d1364881
Ronit Raj <ronitrajofficial7@gmail.com>
2024-04-12 13:40:12 +0530

    prog: made hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import "./App.css";
 import { Hero } from "./components/Hero";
-import { Highlights } from "./components/Highlights";
 import { Navbar } from "./components/Navbar";
-
 function App() {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
-      <Highlights />
     </main>
   );
 }
--- src/components/Highlights.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Highlights = () => {
-  return <div>Highlights</div>;
-};
